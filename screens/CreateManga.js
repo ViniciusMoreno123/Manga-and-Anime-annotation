5d9ea11cea2400229f2dac11730072a311ffc320
ViniciusMoreno123 <93357216+ViniciusMoreno123@users.noreply.github.com>
2023-01-10 15:49:03 -0400

    Add files via upload

--- screens/CreateManga.js
+++ screens/CreateManga.js
@@ -1,8 +1,25 @@
 import React, {Component} from "react";
-import { Text,View,Button } from "react-native";
-
+import { Text,View,Button,TextInput, Alert } from "react-native";
+import db from"../config"
 
 export default class CreateManga extends Component{
+    async addManga(){
+    if(this.state.Manga && this.state.Capitulo && this.state.Observacoes){
+    db.collection("Mangas").add({
+        Manga:this.state.Manga,
+        Capitulo:this.state.Capitulo,
+        Observacoes:this.state.Observacoes
+    })
+    this.setState({
+        Manga:"",
+        Capitulo:"",
+        Observacoes:""
+    })
+    this.props.navigation.navigate("TelaExp")
+    } else{
+        Alert.alert("Falta Feijão nessa comida aí!")
+    }
+    }
 render(){
     return(
      <View style = {{
@@ -13,6 +30,25 @@ render(){
       <Text>
       Criar manga
       </Text>
+      <TextInput
+      style={{borderWidth:2,
+               width:"90%"}}
+      onChangeText= {Manga=>this.setState({Manga})}
+      placeholder={"Insira o nome do mangá"}
+      />
+      <TextInput
+      style={{borderWidth:2,
+               width:"90%"}}
+      onChangeText= {Capitulo=>this.setState({Capitulo})}
+      placeholder={"Insira o número do capítulo"}
+      />
+      <TextInput
+      style={{borderWidth:2,
+               width:"90%"}}
+      onChangeText= {Observacoes=>this.setState({Observacoes})}
+      placeholder={"Insira as observações"}
+      />
+      <Button title="Enviar" onPress={()=> this.addManga()}/>
      </View>   
     )
 
--- screens/TelaManga.js
+++ screens/TelaManga.js
@@ -1,37 +1,57 @@
 import React, {Component} from "react";
-import { Text,View,Button } from "react-native";
+import { Text,View,Button,FlatList } from "react-native";
 
+import db from "../config";
 
 export default class TelaManga extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      mangas:[]
+    }
+  }
+  pegarmangas = ()=>{
+    db.collection("Mangas")
+    .get()
+    .then(snapshot=>{
+      snapshot.docs.map(doc=>{
+      this.setState({
+      mangas: [...this.state.mangas,doc.data()]
+      })
+      })
+    })
+  }
+  componentDidMount = async ()=>{
+    this.pegarmangas()
+  }
+  renderItem = ({item,index})=>{
+    return(
+      <View style = {{borderWidth:2}}>
+         <Text>Mangá:{item.Manga}</Text>
+      <Text> Capitulo do Mangá:{item.Capitulo}</Text>
+      <Text>Observações:{item.Observacoes}</Text> 
+      </View>
+    )
+  }
 render(){
+  const{mangas} = this.state;
     return(
      <View style = {{
     flex:1,
     justifyContent:"center",
     alignItems:"center",
      }}>
-       getTransactions = () => {
-    db.collection("transactions")
-     .get()
-     .then(snapshot =>{
-        snapshot.docs.map(doc=> {
-            this.setState({
-            allTransactions: [...this.state.allTransactions,doc.data()]
-
-            })
-        })
-     })
-   }
-   
-      <View style = {{flex:0.4}}>
-      <Text>
-      Manga
-      </Text>
+      
+     
       <Button title="Criar Manga" onPress={()=> this.props.navigation.navigate("CreateManga")}/>
     
-    </View>
-    <View style = {{flex:0.6}}>
-    
+
+    <View style = {{flex:0.7}}>
+    <FlatList 
+    data={mangas}
+    renderItem={this.renderItem}
+    keyExtractor={(item,index)=> index.toString()}
+    />
     </View>
      </View>   
     )
